9ca79fedcdfd4ad9db304c74b518ec12eb2f2799
Robby Scheer <robertedwinscheer@gmail.com>
2026-09-04 16:23:20 -0700

    fix: Eye1 missing fields

--- client/Game/Items/Eye1Item.cs
+++ client/Game/Items/Eye1Item.cs
@@ -5,7 +5,9 @@ internal static class Eye1Item
     public static readonly ItemDefinition Definition = new()
     {
         Name = "Eye1",
+        ApItemName = "Titania's Eye (Light)",
         PickupLocationName = "1st Class Stateroom - Titania's Eye (Light)",
+        HomeRnvs = new[] { new RoomNodeView(7, 6, 4) },
         DefaultParent = new DefaultParent(null, "CViewItem"),
     };
 }
